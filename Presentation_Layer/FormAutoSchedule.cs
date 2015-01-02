9f38bdb958c8bf06ade9602ad7c1765cbf91767b
thongcs4@gmail.com <thongcs4@gmail.com@098d21a9-f2bf-e564-27fa-140218956333>
2015-01-02 17:15:11 +0000

    Thong remove unnecessary comment

--- Presentation_Layer/FormAutoSchedule.cs
+++ Presentation_Layer/FormAutoSchedule.cs
@@ -190,24 +190,7 @@ namespace Presentation_Layer
         private void btnExport_Click(object sender, EventArgs e)
         {
             Bussiness_Logic_Layer.ExportScheduleToExcel excell_app = new Bussiness_Logic_Layer.ExportScheduleToExcel();
-
-            ////creates the main header
-            //excell_app.createHeaders(5, 2, "Total of Products", "B5", "D5", 2, "YELLOW", true, 10, "n");
-            ////creates subheaders
-            //excell_app.createHeaders(6, 2, "Sold Product", "B6", "B6", 0, "GRAY", true, 10, "");
-            //excell_app.createHeaders(6, 3, "", "C6", "C6", 0, "GRAY", true, 10, "");
-            //excell_app.createHeaders(6, 4, "Initial Total", "D6", "D6", 0, "GRAY", true, 10, "");
-
-            ////add Data to to cells
-            //excell_app.addData(7, 2, "114287", "B7", "B7", "#,##0");
-            //excell_app.addData(7, 3, "", "C7", "C7", "");
-            //excell_app.addData(7, 4, "129121", "D7", "D7", "#,##0");
-            ////add percentage row
-            //excell_app.addData(8, 2, "", "B8", "B8", "");
-            //excell_app.addData(8, 3, "=B7/D7", "C8", "C8", "0.0%");
-            //excell_app.addData(8, 4, "", "D8", "D8", "");
-            ////add empty divider
-            //excell_app.createHeaders(9, 2, "", "B9", "D9", 2, "GAINSBORO", true, 10, "");
+            
         }
 
 
